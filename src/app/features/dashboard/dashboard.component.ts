60ea0316a097c394dfd2a16403ea1c6aca8f9d1b
Adam <adam.zxzzx@gmail.com>
2025-05-10 03:04:14 +0300

    Start dashboard

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,15 +1,27 @@
 import {Component} from '@angular/core';
-import {AppSyncService} from '../../core/grapfql/app-sync.service';
+import {CoinService} from '../../core/grapfql/coin.service';
+import {PromptService} from '../../core/grapfql/prompt.service';
+import {MatGridList, MatGridTile} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButton} from '@angular/material/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    MatGridList,
+    MatGridTile,
+    MatCardModule,
+    MatButton,
+    RouterLink
+
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
-  constructor(appSyncService: AppSyncService) {
+  constructor(private coinService: CoinService, private promptService: PromptService) {
   }
 
 }
